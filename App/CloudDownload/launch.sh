fbff12c1fbd2556962a58caca353b95567671339
CEREAL <hello@shwn.me>
2023-05-15 04:39:41 -0400

    Add files via upload

---
+++ App/CloudDownload/launch.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo $0 $*
+LD_PRELOAD=/mnt/SDCARD/miyoo/lib/libpadsp.so /mnt/SDCARD/.tmp_update/bin/infoPanel -t "Downloading saves" -m "Your saves are being downloaded to your device!" --auto &
+
+cd $(dirname "$0")
+
+/mnt/SDCARD/rclone copy -P -L --no-check-certificate cloud:Onion/saves/retroarch/ /mnt/SDCARD/Saves/CurrentProfile/
---
+++ App/CloudUpload/launch.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo $0 $*
+LD_PRELOAD=/mnt/SDCARD/miyoo/lib/libpadsp.so /mnt/SDCARD/.tmp_update/bin/infoPanel -t "Uploading saves" -m "Your saves are being uploaded to the cloud!" --auto &
+
+cd $(dirname "$0")
+
+/mnt/SDCARD/rclone copy -P -L --no-check-certificate /mnt/SDCARD/Saves/CurrentProfile/ cloud:Onion/saves/retroarch/ 
